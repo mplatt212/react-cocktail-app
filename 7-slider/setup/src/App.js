88b6e4b7ae94be1eeb021525d9b7445a03c55ef9
mplatt212 <mplatthew@gmail.com>
2021-04-04 06:42:06 -0400

    Commit

--- 7-slider/setup/src/App.js
+++ 7-slider/setup/src/App.js
@@ -1,71 +1,73 @@
-import React, { useState, useEffect } from 'react';
-import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
-import { FaQuoteRight } from 'react-icons/fa';
-import data from './data';
+import React, { useState, useEffect } from "react";
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
+import { FaQuoteRight } from "react-icons/fa";
+import data from "./data";
 
 function App() {
-  const [people,setPeople] = useState(data);
-  const [index,setIndex] = useState(0);
+  const [index, setIndex] = useState(0);
+  const [people, setPeople] = useState(data);
 
   useEffect(() => {
-    if(index < 0) {
+    if (index < 0) {
       setIndex(people.length - 1);
     }
-    if(index > people.length - 1) {
+    if (index > people.length - 1) {
       setIndex(0);
     }
-  },[people,index])
+  }, [people, index]);
 
-  useEffect(() => {
-    const slideTime = setInterval(() => {
-      setIndex(index + 1);
-    },5000);
+  const indexPlus = () => {
+    setIndex(index + 1);
+  };
 
-    return () => clearInterval(slideTime);
-  }, [index])
+  const indexMinus = () => {
+    setIndex(index - 1);
+  };
 
-  return <>
-    <section className="section">
-      <div className="title">
-        <h2>
-          <span>/</span>Reviews
-        </h2>
-      </div>
-      <div className="section-center">
-        {people.map((person,personIndex) => {
-          const {id,image,name,title,quote} = person;
+  const setClass = (position) => {
+    if (position === index) {
+      return "person-current";
+    } else if (
+      position === index - 1 ||
+      (position === people.length - 1 && index === 0)
+    ) {
+      return "person-prev";
+    }
 
-          console.log(index)
+    return "person-next";
+  };
 
-          let position = 'slide';
+  return (
+    <section className="section-main">
+      <header>
+        <h1>/Reviews</h1>
+      </header>
+      <article className="carousel">
+        <button className="buttons btn-left" onClick={indexMinus}>
+          <FiChevronLeft />
+        </button>
 
-          if(personIndex === index) {
-            position = 'slide-active';
-          }
+        {people.map((person, personIndex) => {
+          const { image, name, title, quote } = person;
 
-          if(personIndex === index - 1 || (index === 0 && personIndex === people.length - 1)) {
-            position = 'slide-last';
-          }
-          
-          return <>
-            <article key={id} className={position}>
-              <img className='person-img' src={image} alt={name} />
-              <h4 className='person-name'>{name}</h4>
-              <p className="person-title">{title}</p>
-              <p className="person-quote">{quote}</p>
-              <FaQuoteRight className='icon'/>
-            </article>
-          </>
+          return (
+            <section className={setClass(personIndex)} key={personIndex}>
+              <img src={image} alt={name} />
+              <h4>{name}</h4>
+              <h5>{title}</h5>
+              <p>{quote}</p>
+              <div className="quote">
+                <FaQuoteRight />
+              </div>
+            </section>
+          );
         })}
-        <button className="prev" onClick={()=>setIndex(index-1)}>
-          <FiChevronLeft></FiChevronLeft>
+        <button className="buttons btn-right" onClick={indexPlus}>
+          <FiChevronRight />
         </button>
-        <button className="next" onClick={()=>setIndex(index+1)}>
-          <FiChevronRight></FiChevronRight>
-        </button>
-    </div>
+      </article>
     </section>
-  </>
+  );
 }
 
 export default App;
