54bbb5a7fc70819d4523391929b5ef7171aa9785
mplatt212 <mplatthew@gmail.com>
2021-04-05 04:54:24 -0400

    Commit

--- 8-lorem-ipsum/setup/src/App.js
+++ 8-lorem-ipsum/setup/src/App.js
@@ -1,11 +1,49 @@
-import React, { useState } from 'react';
-import data from './data';
+import React, { useState } from "react";
+import data from "./data";
+
 function App() {
-  
-  
+  const [count, setCount] = useState(null);
+  const [text, setText] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (count < 0) {
+      setCount(0);
+    }
+    if (count > 8) {
+      setCount(8);
+    }
+
+    setText(data.slice(0, count));
+  };
+
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <section id="section-main">
+      <h2>Tired of Boring Lorem Ipsum?</h2>
+      <form action="">
+        <label htmlFor="input-ipsum">Paragraphs: </label>
+        <input
+          type="number"
+          min="0"
+          max="8"
+          id="input-ipsum"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button type="submit" onClick={handleSubmit}>
+          GENERATE
+        </button>
+      </form>
+      <article className="text-section">
+        {text.length
+          ? text.map((tex) => {
+              return <p>{tex}</p>;
+            })
+          : ""}
+      </article>
+    </section>
+  );
 }
 
 export default App;
