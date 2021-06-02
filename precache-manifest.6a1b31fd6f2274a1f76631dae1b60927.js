51b7390e0c80b6de9c6a8ec461245fdca94505ee
mplatt212 <mplatthew@gmail.com>
2021-06-01 20:45:57 -0400

    Updates

---
+++ precache-manifest.6a1b31fd6f2274a1f76631dae1b60927.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "748567b80facd75dba4cbcef15d02cc5",
+    "url": "/react-cocktail-app/index.html"
+  },
+  {
+    "revision": "f4cdca991bad51a4f821",
+    "url": "/react-cocktail-app/static/css/main.7e57030a.chunk.css"
+  },
+  {
+    "revision": "8009601e90a0045f2f4e",
+    "url": "/react-cocktail-app/static/js/2.6878d0e2.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react-cocktail-app/static/js/2.6878d0e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4cdca991bad51a4f821",
+    "url": "/react-cocktail-app/static/js/main.c8129e2a.chunk.js"
+  },
+  {
+    "revision": "6d31bfa28972edeeb961",
+    "url": "/react-cocktail-app/static/js/runtime-main.30219d8e.js"
+  },
+  {
+    "revision": "9a3d26451b294fc390aee4ae147a45c7",
+    "url": "/react-cocktail-app/static/media/logo.9a3d2645.svg"
+  }
+]);
